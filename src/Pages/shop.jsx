c27f5ac4616878c28922fbfb297c63d5f5177929
MohammedBetkaoui <mohammed.betkaoui@univ-bba.dz>
2025-01-25 16:02:33 +0100

    modff

--- src/Pages/shop.jsx
+++ src/Pages/shop.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from '../Components/hero/hero'
 import Popular from '../Components/popular/popular'
 import Offres from '../Components/offres/offres'
+import NewCollections from '../Components/NewCollections/newCollections'
  const shop = () => {
   return (
     <div>
         <Hero/>
         <Popular/>
         <Offres/>
+        <NewCollections/>
         
     </div>
   )
